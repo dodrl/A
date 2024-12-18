bb6cb4beefe162d1010da6e643fadcd59c3a8c0f
dodrl <ku3864@gmail.com>
2024-12-18 21:30:21 +0900

    20241218

---
+++ 20241218.py
@@ -0,0 +1,29 @@
+if 10>5:  #조건문만들기 
+    print("correct")
+
+password_correct = True
+if password_correct:
+    print("here is your money")
+else:
+    print("wrong passwoed")
+
+
+password_correct = False
+if password_correct:
+    print("here is your money")
+else:
+    print("wrong passwoed")
+
+
+winner = 10
+
+if winner > 100 :
+    print("winner is greater than 10")
+
+elif winner < 1 :
+    print("winner is  less than 10")
+
+else:
+    print("winner is 10")
+
+
